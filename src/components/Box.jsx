11515e8a9776f3157aca8bbc80d9c5d05c9be021
Kerstin Ohlmann <khaos.player@hotmail.com>
2022-05-03 15:36:40 +0200

    Add box and styled it

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,4 +1,21 @@
-import { format } from "prettier";
-import { useState, useEffect } from "react";
-import styled, {css} from "styled-components";
+import { useState, useEffect } from 'react';
+import styled, { css } from 'styled-components';
 
+export default function Box() {
+  return (
+    <StyledBox>
+      <button>#fhfhdhsfd</button>
+    </StyledBox>
+  );
+}
+
+const StyledBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 200px;
+  background: green;
+  border: 3px solid black;
+`;
