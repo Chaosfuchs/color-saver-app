7d211e062eb08c206cb073cd438a63bf8e4e0260
Kerstin Ohlmann <khaos.player@hotmail.com>
2022-05-04 15:04:03 +0200

    Install npm nano id and add delete function and change function

--- src/App.js
+++ src/App.js
@@ -4,20 +4,40 @@ import ColorBox from './components/ColorBox';
 import ColorForm from './components/ColorForm';
 
 const COLORLIST = [
-  '#F72585',
-  '#B5179E',
-  '#7209B7',
-  '#560BAD',
-  '#480CA8',
-  '#3A0CA3',
-  '#3F37C9',
-  '#4361EE',
-  '#4895EF',
-  '#4CC9F0',
+  { id: 0, hexCode: '#F72585' },
+  { id: 1, hexCode: '#B5179E' },
+  { id: 2, hexCode: '#7209B7' },
+  { id: 3, hexCode: '#560BAD' },
+  { id: 4, hexCode: '#480CA8' },
+  { id: 5, hexCode: '#3A0CA3' },
+  { id: 6, hexCode: '#3F37C9' },
+  { id: 7, hexCode: '#4361EE' },
+  { id: 8, hexCode: '#4895EF' },
+  { id: 9, hexCode: '#4CC9F0' },
 ];
 
 export default function App() {
   const [hexColors, setHexColor] = useState(COLORLIST);
+
+  function handleDelete(colorID) {
+    const newColorList = hexColors.filter(color => color.id !== colorID);
+    setHexColor(newColorList);
+  }
+
+  function handleUpdate(id, newHexCode) {
+    console.log({ id, newHexCode });
+    const newColorList = hexColors.map(color => {
+      if (color.id === id) {
+        return {
+          ...color,
+          hexCode: newHexCode,
+        };
+      } else {
+        return color;
+      }
+    });
+    setHexColor(newColorList);
+  }
 
   function handleSubmit(newColor) {
     const newColors = [newColor, ...hexColors];
@@ -36,7 +56,11 @@ export default function App() {
     <CardFlex>
       <ColorForm onSubmit={handleSubmit} />
       {hexColors.map((color, index) => (
-        <ColorBox key={index} color={color} />
+        <ColorBox
+          key={index}
+          color={color}
+          onDelete={() => console.log(color)}
+        />
       ))}
     </CardFlex>
   );
