fe0c8590198accf61ea01836c166ab9be076db63
Kerstin Ohlmann <khaos.player@hotmail.com>
2022-05-05 09:16:11 +0200

    Add fetching and name lables

--- src/components/ColorBox.jsx
+++ src/components/ColorBox.jsx
@@ -1,8 +1,36 @@
+import { useState, useEffect } from 'react';
 import styled, { css } from 'styled-components';
 
 export default function ColorBox({ color, onChange, onDelete }) {
+  const [fetchedData, setFetchedData] = useState(false);
+  const [colorName, setColorName] = useState('');
+  if (fetchedData) {
+    setTimeout(() => setFetchedData(false, 1000));
+  }
+
+  useEffect(() => {
+    async function fetchName() {
+      try {
+        const response = await fetch(
+          `https://www.thecolorapi.com/id?hex=${color.slice(1)}`
+        );
+        const data = await response.json();
+        setColorName(data.name.value);
+      } catch (message) {
+        console.error(message);
+        setColorName('');
+      }
+    }
+    fetchName();
+  }, [color]);
+
+  function handleClick() {
+    navigator.clipboard.writeText(color);
+  }
+
   return (
     <StyledBox backgroundColor={color} onClick={handleClick}>
+      <span>{colorName}</span>
       <input
         type="text"
         value={color}
@@ -11,9 +39,6 @@ export default function ColorBox({ color, onChange, onDelete }) {
       <button onClick={onDelete}>x</button>
     </StyledBox>
   );
-  function handleClick() {
-    navigator.clipboard.writeText(color);
-  }
 }
 
 const StyledBox = styled.div`
@@ -22,8 +47,10 @@ const StyledBox = styled.div`
   height: 150px;
   border-radius: 15px;
   background: ${props => props.backgroundColor};
-  display: grid;
-  place-content: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   border: 3px solid black;
   cursor: pointer;
 
@@ -31,6 +58,7 @@ const StyledBox = styled.div`
     padding: 5px 10px;
     background-color: #eee;
     border-radius: 10px;
+    text-align: center;
   }
 
   input {
